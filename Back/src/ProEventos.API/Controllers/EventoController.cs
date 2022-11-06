e48c5b1f37ac9f82f2727d46f4c314b4b66220f4
Carlos <carlos.soares@inlog.com.br>
2022-11-06 20:30:21 -0300

    first commit

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Model;
+using System;
+using System.Collections;
+
+namespace ProEventos.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventoController : ControllerBase
+    {
+
+        public EventoController()
+        {
+
+        }
+
+        [HttpGet]
+        public Evento Get()
+        {
+            return new Evento(){
+                EventoId = 1,
+                Tema= "Angular 11 e .NET",
+                Local = "1º lote",
+                QtPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                Lote = "1º lote",
+                ImgEvento = "Foto.png"
+
+            };
+        }
+
+    }
+}
